188e56b71c60abceb14204ffc0118028328221f5
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-01-20 22:56:08 +0100

    [HEADER] adding header in the partial view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+@include('partials.header')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="app.css">
+    <title>E-Commerce Larvel</title>
+</head>
+<body>
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatem harum repellat ipsam veritatis sapiente, soluta voluptates fugiat totam molestias distinctio odit vitae laudantium eveniet repudiandae recusandae dolore sunt tenetur? </div>
+    
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,2 @@
+
+    <header> <div class="header"> <a> Produits </a> <a> Panier </a> <a> Compte </a> </div> </header>
